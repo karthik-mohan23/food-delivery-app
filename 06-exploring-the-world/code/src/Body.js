a8f0ef81f27b602558530988cf37c14fa817d2f3
karthik-mohan23 <karthikmohan2310@gmail.com>
2023-06-16 13:07:37 +0530

    added search functionality

--- 06-exploring-the-world/code/src/Body.js
+++ 06-exploring-the-world/code/src/Body.js
@@ -2,31 +2,34 @@ import { useState, useEffect } from "react";
 
 import RestaurantCard from "./RestaurantCard";
 
-import { RestaurantData } from "../config";
+// import { RestaurantData } from "../config";
 
 function filterData(searchText, restaurants) {
   const searchedRestaurant = restaurants.filter((restaurant) => {
     return restaurant?.strMeal?.includes(searchText);
   });
+  console.log(searchedRestaurant);
   return searchedRestaurant;
 }
 
 const Body = () => {
+  const [searchText, setSearchText] = useState("");
+  const [allRestaurants, setAllRestaurants] = useState([]);
+  const [filterRestaurants, setFilterRestaurants] = useState([]);
+
   async function getRestaurants() {
     const data = await fetch(
       "https://www.themealdb.com/api/json/v1/1/search.php?f=c"
     );
     const json = await data.json();
-    console.log(json.meals);
-    setRestaurants(json.meals);
+    setAllRestaurants(json?.meals);
+    setFilterRestaurants(json?.meals);
   }
 
   useEffect(() => {
     getRestaurants();
-  }, [restaurants]);
+  }, []);
 
-  const [searchText, setSearchText] = useState("");
-  const [restaurants, setRestaurants] = useState(RestaurantData);
   return (
     <div className="section">
       <div>
@@ -39,14 +42,14 @@ const Body = () => {
         />
         <button
           onClick={() => {
-            const data = filterData(searchText, restaurants);
-            setRestaurants(data);
+            const data = filterData(searchText, allRestaurants);
+            setFilterRestaurants(data);
           }}>
           Search
         </button>
       </div>
       <section className="card-container">
-        {restaurants.map((restaurant) => {
+        {filterRestaurants.map((restaurant) => {
           return <RestaurantCard {...restaurant} key={restaurant.idMeal} />;
         })}
       </section>

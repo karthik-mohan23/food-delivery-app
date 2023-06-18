cf0a796f748b9455ab576c514078bbe9a72cd388
karthik-mohan23 <karthikmohan2310@gmail.com>
2023-06-18 12:26:08 +0530

    installed router

--- 07-finding-the-path/code/src/App.js
+++ 07-finding-the-path/code/src/App.js
@@ -1,9 +1,25 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { ReactProvider } from "react-router-dom";
+
+import { createBrowserRouter } from "react-router-dom";
 
 import Header from "./components/Header";
 import Body from "./components/Body";
 import Footer from "./components/Footer";
+import Error from "./components/Error";
+
+const appRouter = createBrowserRouter([
+  {
+    path: "/",
+    element: <AppLayout />,
+    errorElement: <Error />,
+  },
+  {
+    path: "/about",
+    element: <About />,
+  },
+]);
 
 const AppLayout = () => (
   <>
@@ -15,4 +31,4 @@ const AppLayout = () => (
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-root.render(<AppLayout />);
+root.render(<ReactProvider router={appRouter} />);
---
+++ 07-finding-the-path/code/src/components/Error.js
@@ -0,0 +1,8 @@
+const Error = () => {
+  return (
+    <div>
+      <h1>Oops. No page found</h1>
+    </div>
+  );
+};
+export default Error;
